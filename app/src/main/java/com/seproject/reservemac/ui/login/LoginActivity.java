f4967dd27937b6dc8ebdbdacf092bd1dd6d9c855
adityadas8888 <aditya.das8.ad@gmail.com>
2019-09-26 05:50:13 -0500

    sqllite

--- app/src/main/java/com/seproject/reservemac/ui/login/LoginActivity.java
+++ app/src/main/java/com/seproject/reservemac/ui/login/LoginActivity.java
@@ -2,16 +2,21 @@ package com.seproject.reservemac.ui.login;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import com.seproject.reservemac.R;
-
-
+import com.seproject.reservemac.data.model.DatabaseHelpers;
+import com.seproject.reservemac.ui.RegisterActivity;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -21,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
     EditText usernameEditText = null;
     EditText passwordEditText = null;
     Button loginButton = null;
+    Button registerButton = null;
 
 
     @Override
@@ -31,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         usernameEditText = findViewById(R.id.username);
         passwordEditText = findViewById(R.id.password);
         loginButton = findViewById(R.id.login);
+        registerButton = findViewById(R.id.register);
 
         assert connectivityManager != null;
         connection = (connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnectedOrConnecting());
@@ -39,14 +46,23 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                if (connection) {
-                    loginUser();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Switch on the Internet", Toast.LENGTH_LONG).show();
-                }
+//                if (connection) {
+//                    loginUser();
+//                } else {
+//                    Toast.makeText(getApplicationContext(), "Switch on the Internet", Toast.LENGTH_LONG).show();
+//                }
+                readFromDB(usernameEditText.getText().toString(), passwordEditText.getText().toString());
+
             }
         });
 
+    registerButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent myintent = new Intent(getBaseContext(), RegisterActivity.class);
+            startActivity(myintent);
+        }
+    });
 
     }
 
@@ -72,6 +88,57 @@ public class LoginActivity extends AppCompatActivity {
         }
 
 
+
+    }
+
+
+
+
+    private void readFromDB(String user, String password) {
+        String username="";
+        String role="";
+        String pass="";
+
+        SQLiteDatabase database = new DatabaseHelpers(this).getReadableDatabase();
+
+        String[] projection = {
+                DatabaseHelpers.ROLE,
+                DatabaseHelpers.USERNAME,
+           DatabaseHelpers.PASSWORD
+
+        };
+
+        String selection =
+                DatabaseHelpers.USERNAME + " = '"+user+"' and " +
+                        DatabaseHelpers.PASSWORD + " = '"+password+"'";
+
+//        String[] selectionArgs = {"%" + Task_name + "%", Task_Category, "%" + Task_Description + "%" + Task_Date + "%" + Task_Time + "%" + Snooze_Time + "%"};
+
+
+        try {
+            Cursor cursor = database.query(
+                    DatabaseHelpers.USER_TABLE_NAME,   // The table to query
+                    projection,                               // The columns to return
+                    selection,                                // The columns for the WHERE clause
+                    null,                            // The   for the WHERE clause
+                    null,                                     // don't group the rows
+                    null,                                     // don't filter by row groups
+                    null                                      // don't sort
+            );
+            if (cursor.moveToFirst()) {
+                do {
+                     role = ((cursor.getString(0)));   // ID //
+                     username = (cursor.getString(1));
+                     pass = (cursor.getString(2));
+
+
+                } while (cursor.moveToNext());
+            }
+            Toast.makeText(this, "user:"+username+" role: "+role, Toast.LENGTH_SHORT).show();
+            Log.d("TAG", "The total cursor count is " + cursor.getCount());
+        } catch (Exception e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
