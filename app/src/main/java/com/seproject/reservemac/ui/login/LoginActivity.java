3e4abd7a9b6e1bf8e95071d4dedf0c395187521d
adityadas8888 <aditya.das8.ad@gmail.com>
2019-09-25 22:23:55 -0500

    fixed login builder class

--- app/src/main/java/com/seproject/reservemac/ui/login/LoginActivity.java
+++ app/src/main/java/com/seproject/reservemac/ui/login/LoginActivity.java
@@ -3,26 +3,20 @@ package com.seproject.reservemac.ui.login;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import com.seproject.reservemac.R;
 
-import java.util.HashMap;
+
 
 
 public class LoginActivity extends AppCompatActivity {
 
     private boolean connection = false;
-    private static String url = null;
-    private static String loginurl = "http://zenithwrites.org/login.php";
-
 
     EditText usernameEditText = null;
     EditText passwordEditText = null;
@@ -59,27 +53,26 @@ public class LoginActivity extends AppCompatActivity {
 
     public void loginUser() {
 
-
-        String username = usernameEditText.getText().toString().trim().toLowerCase();
+        String password = passwordEditText.getText().toString().trim();
+        String username = usernameEditText.getText().toString().trim();
         if (usernameEditText.getText().toString().trim().equals("")) {
             usernameEditText.setError("The username is required");
 
             usernameEditText.setHint("Please enter your username");
-        }
-
-        String password = passwordEditText.getText().toString().trim().toLowerCase();
-
-        if (passwordEditText.getText().toString().trim().equals("")) {
+        } else if (passwordEditText.getText().toString().trim().equals("")) {
             passwordEditText.setError("Password is required!");
 
             passwordEditText.setHint("Please enter your Password");
         }
 
-        login(username, password);
+        else{
+            Toast.makeText(this, username+password,Toast.LENGTH_SHORT).show();
+            String type = "login";
+            Backgroundworker backgroundworker = new Backgroundworker(this);
+            backgroundworker.execute(type,username,password);
+        }
 
-    }
 
-    private void login(final String username, final String password) {
-        new LoginUserClass(this).execute(username,password);
+
     }
 }
