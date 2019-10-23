f27bc051fa9c713f273545ea06928d3db1a63aa8
Murtuza24 <murtuzabhaiji@gmail.com>
2019-10-23 15:29:29 -0500

    UC 13,14,15 and admins

---
+++ app/src/main/java/com/seproject/reservemac/ListOfFacilitiesActivity.java
@@ -0,0 +1,14 @@
+package com.seproject.reservemac;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListOfFacilitiesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_facilities);
+    }
+}
---
+++ app/src/main/java/com/seproject/reservemac/SearchFacilityActivity.java
@@ -0,0 +1,17 @@
+package com.seproject.reservemac;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SearchFacilityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_facility);
+
+
+
+    }
+}
