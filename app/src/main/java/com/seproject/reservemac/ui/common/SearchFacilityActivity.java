fc84b65b840363c52515f13802b5d2d88d33cdde
adityadas8888 <aditya.das8.ad@gmail.com>
2019-11-24 12:59:31 -0600

    Made the TimePickerDialog

--- app/src/main/java/com/seproject/reservemac/ui/common/SearchFacilityActivity.java
+++ app/src/main/java/com/seproject/reservemac/ui/common/SearchFacilityActivity.java
@@ -40,11 +40,6 @@ public class SearchFacilityActivity extends AppCompatActivity implements  DatePi
         BtnSearchFacility = findViewById(R.id.BtnSearchFacility);
         BtnDate = findViewById(R.id.EtxDatePicker);
         BtnTime = findViewById(R.id.EtxTimePicker);
-//        String[] items = new String[]{"00:00", "01:00","02:00", "03:00","04:00", "05:00","06:00", "07:00","08:00", "09:00","10:00", "11:00","12:00", "13:00","14:00", "15:00","16:00", "17:00","18:00", "19:00","20:00", "21:00","22:00", "23:00"};
-//        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
-//        SpinnerTime.setSelection(0);
-//        SpinnerTime.setAdapter(adapter);
-
         BtnSearchFacility.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
