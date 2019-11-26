a749c87ebae5288bdcf1b681cb9952eabbbb8247
Murtuza24 <murtuzabhaiji@gmail.com>
2019-11-26 07:48:32 -0600

    delete and report violation

--- app/src/main/java/com/seproject/reservemac/facility_manager/Facility_manager_reservation.java
+++ app/src/main/java/com/seproject/reservemac/facility_manager/Facility_manager_reservation.java
@@ -1,10 +1,12 @@
 package com.seproject.reservemac.facility_manager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +14,7 @@ import com.seproject.reservemac.R;
 import com.seproject.reservemac.background.GetRequests;
 import com.seproject.reservemac.model.FacilityModel;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -46,6 +49,24 @@ public class Facility_manager_reservation extends AppCompatActivity implements G
         reservationId = getIntent().getStringExtra("ReservationId");
 
         loadData();
+
+        DeleteReservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                http://mohammedmurtuzabhaiji.uta.cloud/se1project/fm/delete_reservation.php?reservationid=102
+
+                String type = "DeleteReservation";
+                StringBuilder stringBuilder = new StringBuilder();
+                final FacilityModel facilityModel = getIntent().getParcelableExtra("facilityModel");
+                stringBuilder.append("fm/delete_reservation.php?reservationid=").append(reservationId);
+
+                String url = stringBuilder.toString();
+                new GetRequests(Facility_manager_reservation.this, url, Facility_manager_reservation.this, "DeleteReservation").execute("");
+
+
+            }
+        });
+
         ReportViolation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,7 +84,7 @@ public class Facility_manager_reservation extends AppCompatActivity implements G
                         stringBuilder.append("fm/report_violation.php?reservationid=").append(reservationId);
                         stringBuilder.append("&viodetails=").append(violations);
                         String url = stringBuilder.toString();
-                        new GetRequests(Facility_manager_reservation.this, url, Facility_manager_reservation.this, "report violations").execute("");
+                        new GetRequests(Facility_manager_reservation.this, url, Facility_manager_reservation.this, "ReportViolation").execute("");
 
 
                     }
@@ -93,16 +114,50 @@ public class Facility_manager_reservation extends AppCompatActivity implements G
 
         if (jsonObject != null) {
 
-            try {
-                JSONObject jsonContent = jsonObject.getJSONObject("content");
-                ReservationId.setText(String.valueOf(jsonContent.getString("reservationid")));
-                username.setText(String.valueOf(jsonContent.getString("username")));
-                FacilityCode.setText(String.valueOf(jsonContent.getString("name")));
-                Bookingtime.setText(String.valueOf(jsonContent.getString("time")));
+            if (Identity.equalsIgnoreCase("SearchReservation")) {
+
+                try {
+                    JSONArray jsonContent = jsonObject.getJSONArray("content");
+                    JSONObject json = jsonContent.getJSONObject(0);
+                    ReservationId.setText(String.valueOf(json.getString("reservationid")));
+                    username.setText(String.valueOf(json.getString("username")));
+                    FacilityCode.setText(String.valueOf(json.getString("name")));
+                    Bookingtime.setText(String.valueOf(json.getString("time")));
 
 
-            } catch (JSONException e) {
-                e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else if (Identity.equalsIgnoreCase("DeleteReservation")) {
+                try {
+                    String resp = jsonObject.getString("response_code");
+                    if (resp.equalsIgnoreCase("OK")) {
+                        Toast.makeText(this, "Reservation Deleted.", Toast.LENGTH_SHORT).show();
+                    }
+                    Intent i = new Intent(Facility_manager_reservation.this, SearchReservation.class);
+                    i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//**Change Here**
+                    startActivity(i);
+                    finish();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                String resp = null;
+                try {
+                    resp = jsonObject.getString("response_code");
+
+                    if (resp.equalsIgnoreCase("OK")) {
+                        Toast.makeText(this, "Reservation Deleted.", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Intent i = new Intent(Facility_manager_reservation.this, SearchReservation.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);//**Change Here**
+                startActivity(i);
+                finish();
             }
 
         }
